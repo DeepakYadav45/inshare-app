fe7f8165abb8957dfe0752001ba391babcdb8258
deepak.my89@gmail.com <91742463+DeepakYadav45@users.noreply.github.com>
2022-08-20 13:14:54 +0530

    updation of css

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 app.use(express.json());
 
 const connectDB = require('./config/db');
