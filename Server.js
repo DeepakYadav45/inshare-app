ef25cc18549c4bc33e809f01acae5ad08ba45f16
deepak.my89@gmail.com <91742463+DeepakYadav45@users.noreply.github.com>
2022-08-20 14:13:34 +0530

    update

--- Server.js
+++ Server.js
@@ -3,21 +3,6 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-        // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
-
-app.use(cors(corsOptions))
-
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -27,6 +12,13 @@ connectDB();
 //cors
 
 
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+        // ['http://localhost:3001', 'http://localhost:5000', 'http://localhost:3300']
+}
+
+
+app.use(cors(corsOptions));
 //Templattes engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
