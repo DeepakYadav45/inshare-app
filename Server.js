24018e6cfa0b2815fa8d8aa4c1f0168f10a88b25
deepak.my89@gmail.com <91742463+DeepakYadav45@users.noreply.github.com>
2022-08-21 14:31:36 +0530

    css

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
 const connectDB = require('./config/db');
